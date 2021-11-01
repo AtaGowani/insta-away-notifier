c9e2a50109f8a8770dc615e6492bc90d47f79b2d
Ata Gowani <atagowani@gmail.com>
2021-10-31 22:48:50 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+## Instagram Away Notifier
+A selenium job to go through instagram unread messages and send a custom automated message. This script helps put an end to the FOMO while you're taking a break from instagram.
+
+### Setup
+* Populate `.env`
+* Set up `crontab` to run this job every so often
---
+++ job.py
@@ -0,0 +1,90 @@
+import time
+import os
+from selenium import webdriver
+from time import sleep
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MESSAGE = os.getenv("MESSAGE")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+SHOULD_RUN = os.getenv("SHOULD_RUN")
+
+def check_unread_and_notify():
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.add_argument("--headless")
+    driver = webdriver.Firefox(options=firefox_options)
+
+    driver.get('https://www.instagram.com/')
+    driver.implicitly_wait(15)
+    print("Opened Instagram...")
+
+    username = driver.find_element("name", "username")
+    username.send_keys(USERNAME)
+    print("Username entered...")
+
+    password = driver.find_element("name", "password")
+    password.send_keys(PASSWORD)
+    print("Password entered...")
+
+    button = driver.find_element("xpath", "//button[@type='submit']")
+    button.click()
+
+    # Don't save the password
+    button = driver.find_element(
+        "xpath", "//button[contains(text(), 'Not Now')]")
+    button.click()
+
+    # Don't turn on notifications
+    button = driver.find_element(
+        "xpath", "//button[contains(text(), 'Not Now')]")
+    button.click()
+    print("Instagram opened...")
+
+    links = driver.find_elements("tag name", "a")
+    click_messages_tab(links)
+    time.sleep(5)  # Wait for messages to load
+
+    messages = driver.find_elements(
+        "xpath", "//div[contains(@aria-label, 'Unread')]")
+    if messages:
+        respond_to_unread(messages, driver)
+    else:
+        print("No unread messages found.")
+
+    driver.close()
+    print("Done")
+
+
+def respond_to_unread(messages, driver):
+    while(messages):
+        try:
+            messages[0].click()
+
+            text_box = driver.find_element(
+                "xpath", "//textarea[contains(@placeholder, 'Message...')]")
+            text_box.send_keys(MESSAGE)
+
+            send_button = driver.find_element(
+                "xpath", "//button[contains(text(), 'Send')]")
+            send_button.click()
+
+            messages = driver.find_elements(
+                "xpath", "//div[contains(@aria-label, 'Unread')]")
+        except BaseException as err:
+            print("Unexpected" + err + " " + type(err))
+
+
+def click_messages_tab(tab_links):
+    for link in tab_links:  # Go through links and find the message one
+        try:
+            if link.get_attribute("href") == "https://www.instagram.com/direct/inbox/":
+                link.click()
+                break
+        except BaseException as err:
+            print("Unexpected" + err + " " + type(err))
+            print("Direct messages opened...")
+
+if SHOULD_RUN:
+    check_unread_and_notify()
